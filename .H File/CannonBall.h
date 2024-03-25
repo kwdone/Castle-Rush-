cafb1d6447816aea46393620f336083bd26fcea6
Nguyen Dang Khoa <154901435+kwdone@users.noreply.github.com>
2024-03-25 09:01:11 +0700

    Header file for cannon balls

---
+++ .H File/CannonBall.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <memory>
+#include "SDL.h"
+#include "Vector2D.h"
+#include "TextureLoader.h"
+#include "Unit.h"
+
+class CannonBall
+{
+public:
+    CannonBall(SDL_Renderer* renderer, Vector2D setPos, Vector2D setDirectionNormal);
+    void update(float dT, std::vector<std::shared_ptr<Unit>>& listUnits);
+    void draw(SDL_Renderer* renderer, int tileSize);
+    bool getCollisionOccurred();
+private:
+    void checkCollision(std::vector<std::shared_ptr<Unit>>& listUnits);
+    Vector2D pos, directionNormal;
+    static const float speed, size, distanceTraveledMax;
+    float distanceTraveled = 0.0f;
+
+    SDL_Texture* texture = nullptr;
+
+    bool collisionOccurred = false;
+};
+
