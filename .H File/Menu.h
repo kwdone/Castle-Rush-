de7bce26c5dcbfcade0a375f94dee0eb1de7673a
Nguyen Dang Khoa <154901435+kwdone@users.noreply.github.com>
2024-04-19 16:16:38 +0700

    Update Menu.h

--- .H File/Menu.h
+++ .H File/Menu.h
@@ -8,6 +8,8 @@ class Menu{
     public:
     //Menu(SDL_Surface* screen, SDL_Renderer* renderer, TTF_Font* font);
     TTF_Font* font = NULL;
-   int showMenu(SDL_Window* window, SDL_Renderer* renderer, TTF_Font* font);
-
+    TTF_Font* fontSmaller = TTF_OpenFont("C:/Stuff/Pixelated.ttf", 60);
+    int showMenu(SDL_Window* window, SDL_Renderer* renderer, TTF_Font* font);
+    void renderText(SDL_Renderer* renderer, float p_x, float p_y, const char* p_text, TTF_Font* font, SDL_Color textColor);
+    const char* getHighScoreText();
 };
