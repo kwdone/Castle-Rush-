b1f56132ac152491267ae818190e3cdf4120bd87
Nguyen Dang Khoa <154901435+kwdone@users.noreply.github.com>
2024-04-19 16:17:58 +0700

    Update Level.h

--- .H File/Level.h
+++ .H File/Level.h
@@ -5,20 +5,24 @@
 #include "SDL.h"
 #include "Vector2D.h"
 #include "TextureLoader.h"
+#include "Timer.h"
 
 
 class Level
 {
-private:
+    public:
     enum class TileType : char {
         empty,
         wall,
         enemySpawner,
         edge,
+        portal,
+        no_tower,
         bush_occupied,
         tree_occupied,
-        no_tower
+        greenPortal
         };
+    private:
 	static const unsigned char flowDistanceMax = 255;
 
 	struct Tile {
@@ -32,8 +36,11 @@ private:
 public:
 	Level(SDL_Renderer* renderer, int setTileCountX, int setTileCountY);
 	void draw(SDL_Renderer* renderer, int tileSize);
+	void drawWithPortal(SDL_Renderer* renderer, int tileSize, int roundsSpawned);
+	void drawWithAnimation(SDL_Renderer* renderer, int pos_x, int pos_y, SDL_Texture* object);
 	void drawObject(SDL_Renderer* renderer, int pos_x, int pos_y, SDL_Texture* object);
-
+    void teleportUnit(Vector2D& unitPosition);
+    void teleportUnitGreen(Vector2D& unitPosition);
     Vector2D getRandomEnemySpawnerLocation();
     bool isTilePath(int x, int y);
 	bool isTileWall(int x, int y);
@@ -43,24 +50,57 @@ public:
 	void setTileWall(int x, int y, bool setWall);
 	Vector2D getTargetPos();
 	Vector2D getFlowNormal(int x, int y);
-
-
-private:
+    static int chooseLevel;
+    static bool openPortal;
     TileType getTileType(int x, int y);
+private:
+    Timer timerForLevel3;
+    SDL_Texture* texturePortal;
+    SDL_Texture* textureGreenPortal;
+    Vector2D portalDestinations[4];
+    //TileType getTileType(int x, int y);
     void setTileType(int x, int y, TileType tileType);
 	void drawTile(SDL_Renderer* renderer, int x, int y, int tileSize);
 	void calculateFlowField();
 	void calculateDistances();
 	void calculateFlowDirections();
-    void load_map();
+    void load_map(std::vector<int> Types);
 ///////
 	std::vector<Tile> listTiles;
 
     //std::vector<int> Types = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 ,0 ,0 ,0 ,0};
-	const int tileCountX, tileCountY;
+    const int tileCountX, tileCountY;
+	int targetX = 0, targetY = 0;
 
-	const int targetX = 0, targetY = 0;
+    std::vector<int> Types1 = {0, 1, 1, 1, 1, 4, 4, 4, 4, 2, 1, 1, 1, 1, 0,
+                              0, 0, 0, 0, 1, 4, 4, 4, 2, 1, 3, 1, 1, 0, 0,
+                              0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 3, 0, 0,
+                              1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1,
+                              1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1,
+                              1, 0, 1, 0, 4, 4, 1, 0, 4, 4, 1, 0, 1, 0, 1,
+                              1, 0, 0, 0, 4, 4, 4, 1, 4, 4, 1, 0, 0, 0, 1,
+                              1, 0, 1, 4, 4, 4, 4, 4, 4, 3, 1, 0, 1, 0, 1,
+                              0, 0, 1, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 0, 0};
 
+    std::vector<int> Types2 = {0, 0, 1, 1, 1, 4, 4, 4, 4, 4, 1, 1, 1, 1, 0,
+                              1, 0, 0, 0, 0, 1, 4, 4, 4, 1, 0, 0, 0, 0, 0,
+                              1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1,
+                              1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1,
+                              1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
+                              1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1,
+                              1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1,
+                              0, 0, 1, 1, 4, 4, 4, 4, 4, 4, 4, 1, 1, 0, 0,
+                              0, 0, 1, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 0, 1};
+
+    std::vector<int> Types3 = {0, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 0,
+                              0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0,
+                              0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0,
+                              0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0,
+                              0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0,
+                              0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0,
+                              0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0,
+                              0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+                              0, 1, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 1, 1, 1};
 	SDL_Texture* textureTileWall = nullptr,
 		*textureTileTarget = nullptr,
 		*textureTileEnemySpawner = nullptr,
@@ -76,5 +116,13 @@ private:
 		*textureBush = nullptr,
 		*textureTree = nullptr,
 		*textureTreeShadow = nullptr,
-		*textureRock = nullptr;
+		*textureRock = nullptr,
+		*textureRuin = nullptr,
+		*textureRuinStair = nullptr,
+		*textureCamp3 = nullptr,
+		*textureCamp4 = nullptr,
+		*textureFlower = nullptr,
+		*textureGrass = nullptr,
+		*textureCampFire = nullptr,
+		*textureFlag = nullptr;
 };
